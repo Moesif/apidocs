373490fcf95cd5811711df01ece381c863784370
Derric <derric@moesif.com>
2019-01-23 13:48:09 -0800

    Change URL for GitHub Pages

--- config.rb
+++ config.rb
@@ -83,7 +83,7 @@ configure :build do
   activate :minify_javascript
   # activate :relative_assets
   activate :asset_hash
-  activate :asset_host, :host => '//moesif.github.io/apidocs'
+  activate :asset_host, :host => '//apidocs.moesif.com'
   # activate :gzip
 end
 
